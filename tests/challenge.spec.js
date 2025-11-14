a54e4b3d96149673825cd3a08993fb26a8c1d20f
Alex Zarin <vector-777@yandex.ru>
2025-11-14 12:53:18 +0300

    add 33 post test

--- tests/challenge.spec.js
+++ tests/challenge.spec.js
@@ -556,4 +556,25 @@ test.describe("Tsets for APIchallenge", () => {
         expect(headers["content-type"]).toContain("application/json");
     });
 
+    test("33 POST /todos (415)", async ( { request } ) => {
+        let response = await request.post(`${URL}/todos`, {
+            headers: {
+                "x-challenger": token,
+                "Content-Type": 'gzip',
+                "accept": "application/json",
+            },
+            data: {
+                doneStatus: true,
+                title: 'one two three',
+                description: 'bla',
+            }
+            });
+
+        let headers = response.headers();
+
+        expect(response.status()).toBe(415);
+        expect(headers).toEqual(expect.objectContaining({ "x-challenger": token }));
+        expect(headers["content-type"]).toContain("application/json");
+    });
+
 });
